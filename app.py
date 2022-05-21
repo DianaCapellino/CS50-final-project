fc8b131e3037e9f959cd4dcad7d9d5101358da37
KoalaDiana <diana.capellino@gmail.com>
2022-05-21 12:22:23 -0300

    Gave the first basic style

--- app.py
+++ app.py
@@ -7,13 +7,11 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
+from helpers import error, login_required
+
 
 # Configure application
 app = Flask(__name__)
-
-
-if __name__ == '__main__':
-    app.run()
 
 
 # Ensure templates are auto-reloaded
@@ -25,7 +23,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///goldendot.db")
 
 @app.after_request
 def after_request(response):
@@ -38,7 +36,7 @@ def after_request(response):
 
 @app.route("/")
 def index():
-    return flash("Checking")
+    return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -52,18 +50,18 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            flash("Please provide Username")
+            return error("Please provide Username")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            flash("Please provide Password")
+            return error("Please provide Password")
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            flash("invalid username and/or password")
+            return error("invalid username and/or password")
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -98,27 +96,28 @@ def register():
         username = request.form.get("username")
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
+        email = request.form.get("email")
 
-        # Check if the username or passwords are not blank
-        if not username or not password:
-            return apology("Username or Password are missing. Please complete both labels.", 400)
+        # Check if the username, email or passwords are not blank
+        if not username or not password or not email:
+            return error("Username or Password are missing. Please complete both labels.")
 
         # Check if password matches confirmation
         elif password != confirmation:
-            return apology("Passwords do not match", 400)
+            return error("Passwords do not match")
 
         # Require to the user to input numbers and letters
         if password.isnumeric() or len(password) < 6:
-            return apology("Please provide a password with a minimum lenght of 6 digits and at least 1 letter or symbol ")
+            return error("Please provide a password with a minimum lenght of 6 digits and at least 1 letter or symbol ")
 
         # Generate hash for password
         pwhash = generate_password_hash(password)
 
         # Register user into the database only if it is unique
         try:
-            db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, pwhash)
+            db.execute("INSERT INTO users (username, hash, email) VALUES (?, ?, ?)", username, pwhash, email)
         except:
-            return apology("Username already exists. Please select a different one.", 400)
+            return error("Username already exists. Please select a different one.")
 
         # Return the user to the login page
         return redirect("/login")
@@ -126,5 +125,3 @@ def register():
     # If they haven't registered yet it shows the register template
     else:
         return render_template("register.html")
-
-
